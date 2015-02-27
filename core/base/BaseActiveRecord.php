4571efb532121ef33c29ed8d966ce07b4248eb1a
Jia Guo <310255981@qq.com>
2015-02-27 12:49:07 +0800

    初始化2

---
+++ core/base/BaseActiveRecord.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\core\base;
+
+use Yii;
+use app\models\User;
+use yii\db\ActiveRecord;
+
+
+/**
+ * UserController implements the CRUD actions for User model.
+ */
+class BaseActiveRecord extends ActiveRecord
+{
+
+}
---
+++ core/base/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015-02-27
+ * Time: 11:16
+ */
+
+namespace app\core\base;
+
+
+use yii\web\Controller;
+
+class BaseController extends Controller {
+
+}
